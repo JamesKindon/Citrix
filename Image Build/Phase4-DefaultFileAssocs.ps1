2079035172b72b5a82797d1c5fac4902712c99ee
JamesKindon <James.Kindon@gmail.com>
2020-02-07 23:20:37 +1100

    Update Build

---
+++ Image Build/Phase4-DefaultFileAssocs.ps1
@@ -0,0 +1,134 @@
+<# 
+    .SYNOPSIS
+        Customise a Windows Server image for use as an RDS/XenApp VM in Azure.
+#>
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $False)]
+    [System.String] $Log = "$env:SystemRoot\Logs\AzureArmCustomDeploy.log",
+
+    [Parameter(Mandatory = $False)]
+    [System.String] $Target = "$env:SystemDrive\Apps"
+)
+
+#region Functions
+Function Set-Repository {
+    # Trust the PSGallery for modules
+    If (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+        Write-Verbose "Trusting the repository: PSGallery"
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
+}
+
+Function Invoke-Process {
+    <#PSScriptInfo 
+    .VERSION 1.4 
+    .GUID b787dc5d-8d11-45e9-aeef-5cf3a1f690de 
+    .AUTHOR Adam Bertram 
+    .COMPANYNAME Adam the Automator, LLC 
+    .TAGS Processes 
+    #>
+
+    <# 
+    .DESCRIPTION 
+    Invoke-Process is a simple wrapper function that aims to "PowerShellyify" launching typical external processes. There 
+    are lots of ways to invoke processes in PowerShell with Invoke-Process, Invoke-Expression, & and others but none account 
+    well for the various streams and exit codes that an external process returns. Also, it's hard to write good tests 
+    when launching external proceses. 
+ 
+    This function ensures any errors are sent to the error stream, standard output is sent via the Output stream and any 
+    time the process returns an exit code other than 0, treat it as an error. 
+    #> 
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $FilePath,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $ArgumentList
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    try {
+        $stdOutTempFile = "$env:TEMP\$((New-Guid).Guid)"
+        $stdErrTempFile = "$env:TEMP\$((New-Guid).Guid)"
+
+        $startProcessParams = @{
+            FilePath               = $FilePath
+            ArgumentList           = $ArgumentList
+            RedirectStandardError  = $stdErrTempFile
+            RedirectStandardOutput = $stdOutTempFile
+            Wait                   = $true
+            PassThru               = $true
+            NoNewWindow            = $true
+        }
+        if ($PSCmdlet.ShouldProcess("Process [$($FilePath)]", "Run with args: [$($ArgumentList)]")) {
+            $cmd = Start-Process @startProcessParams
+            $cmdOutput = Get-Content -Path $stdOutTempFile -Raw
+            $cmdError = Get-Content -Path $stdErrTempFile -Raw
+            if ($cmd.ExitCode -ne 0) {
+                if ($cmdError) {
+                    throw $cmdError.Trim()
+                }
+                if ($cmdOutput) {
+                    throw $cmdOutput.Trim()
+                }
+            }
+            else {
+                if ([System.String]::IsNullOrEmpty($cmdOutput) -eq $false) {
+                    Write-Output -InputObject $cmdOutput
+                }
+            }
+        }
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+    finally {
+        Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force -ErrorAction Ignore
+    }
+}
+
+Function Install-CoreApps {
+    # Set TLS to 1.2
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    #region Modules
+    Write-Host "=========== Installing required modules"
+    # Install the Evergreen module
+    # https://github.com/aaronparker/Evergreen
+    Install-Module -Name Evergreen -AllowClobber
+    #endregion
+
+    #region Default Apps & File Type Associations
+    $url = "https://raw.githubusercontent.com/JamesKindon/Citrix/master/Image%20Build/Default%20File%20Assocs/FileTypeAssociations%20(Edge).xml"
+    $output = "$Target\$(Split-Path -Path $url -Leaf)"
+    Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing
+    Invoke-Process -FilePath "$env:SystemRoot\System32\dism.exe" -ArgumentList "/Online /Import-DefaultAppAssociations:$output" -Verbose
+    Remove-Variable -Name url
+    #endregion
+
+}
+#endregion
+
+
+#region Script logic
+# Start logging
+Write-Host "=========== Running: $($MyInvocation.MyCommand)."
+Start-Transcript -Path $Log -Append
+
+# If local path for script doesn't exist, create it
+If (!(Test-Path $Target)) { New-Item -Path $Target -ItemType Directory -Force -ErrorAction SilentlyContinue }
+
+# Run tasks
+Set-Repository
+Install-CoreApps
+
+# Stop Logging
+Stop-Transcript
+Write-Host "=========== Complete: $($MyInvocation.MyCommand)."
+#endregion
