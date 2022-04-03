8687d5175e01b3c502ffc5d800b671009d327dad
James Kindon <James.Kindon@gmail.com>
2022-04-04 08:58:48 +1000

    Functions

--- Sealing/Connectwise/ConnectWiseControlPers.ps1
+++ Sealing/Connectwise/ConnectWiseControlPers.ps1
@@ -23,6 +23,88 @@ param (
 # Functions
 # ============================================================================
 #region Functions
+function Write-Log {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("LogContent")]
+        [string]$Message,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('LogPath')]
+        [string]$Path = $LogPath,
+        
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Error", "Warn", "Info")]
+        [string]$Level = "Info",
+        
+        [Parameter(Mandatory = $false)]
+        [switch]$NoClobber
+    )
+
+    Begin {
+        # Set VerbosePreference to Continue so that verbose messages are displayed.
+        $VerbosePreference = 'Continue'
+    }
+    Process {
+        
+        # If the file already exists and NoClobber was specified, do not write to the log.
+        if ((Test-Path $Path) -AND $NoClobber) {
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name."
+            Return
+        }
+
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path.
+        elseif (!(Test-Path $Path)) {
+            Write-Verbose "Creating $Path."
+            $NewLogFile = New-Item $Path -Force -ItemType File
+        }
+
+        else {
+            # Nothing to see here yet.
+        }
+
+        # Format Date for our Log File
+        $FormattedDate = Get-Date -Format "dd-MM-yyyy HH:mm:ss" #this is in AU time
+
+        # Write message to error, warning, or verbose pipeline and specify $LevelText
+        switch ($Level) {
+            'Error' {
+                Write-Error $Message
+                $LevelText = 'ERROR:'
+            }
+            'Warn' {
+                Write-Warning $Message
+                $LevelText = 'WARNING:'
+            }
+            'Info' {
+                Write-Verbose $Message
+                $LevelText = 'INFO:'
+            }
+        }
+        
+        # Write log entry to $Path
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append
+    }
+    End {
+    }
+}
+
+function RollOverlog {
+    $LogFile = $LogPath
+    $LogOld = Test-Path $LogFile -OlderThan (Get-Date).AddDays(-$LogRollover)
+    $RolloverDate = (Get-Date -Format "dd-MM-yyyy")
+    if ($LogOld) {
+        Write-Log -Message "$LogFile is older than $LogRollover days, rolling over" -Level Info
+        $NewName = [io.path]::GetFileNameWithoutExtension($LogFile)
+        $NewName = $NewName + "_$RolloverDate.log"
+        Rename-Item -Path $LogFile -NewName $NewName
+        Write-Log -Message "Old logfile name is now $NewName" -Level Info
+    }    
+}
 #endregion
 
 # ============================================================================
